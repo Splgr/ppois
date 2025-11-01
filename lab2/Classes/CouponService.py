4152f7fcfb6b53cbce0da278e8db4ce54d8204b1
Alina <grigorcikalina@gmail.com>
2025-11-01 11:45:48 +0300

    Update CouponService.py

--- lab2/Classes/CouponService.py
+++ lab2/Classes/CouponService.py
@@ -4,8 +4,7 @@ from PercentageCoupon import PercentageCoupon
 from FixedAmountCoupon import FixedAmountCoupon
 
 class CouponService:
-    @staticmethod
-    def validate_and_apply(coupon: Coupon, order_amount: float) -> float:
+    def validate_and_apply(self, coupon: Coupon, order_amount: float) -> float:
         if not coupon.is_valid():
             raise Exception("Купон недействителен")
         
@@ -14,12 +13,10 @@ class CouponService:
             
         return coupon.apply(order_amount)
     
-    @staticmethod
-    def create_percentage_coupon(discount_percent: float, days_valid: int = 30) -> PercentageCoupon:
+    def create_percentage_coupon(self, discount_percent: float, days_valid: int = 30) -> PercentageCoupon:
         expiry_date = datetime.now() + timedelta(days=days_valid)
         return PercentageCoupon(discount_percent, expiry_date)
     
-    @staticmethod
-    def create_fixed_coupon(discount_amount: float, min_order_amount: float = 0, days_valid: int = 30) -> FixedAmountCoupon:
+    def create_fixed_coupon(self, discount_amount: float, min_order_amount: float = 0, days_valid: int = 30) -> FixedAmountCoupon:
         expiry_date = datetime.now() + timedelta(days=days_valid)
         return FixedAmountCoupon(discount_amount, min_order_amount, expiry_date)
